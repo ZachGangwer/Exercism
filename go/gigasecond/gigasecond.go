6d78f9917632b883fd7a88145df6ba2b99e7455e
Zach Gangwer <gangwerz@work.zmania>
2017-05-10 14:23:45 -0700

    clock worked on

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -5,4 +5,22 @@ import "time"
 
 const testVersion = 4
 
-func AddGigasecond(time.Time) time.Time
+func timeAdd(base time.Time, mod time.Time) time.Time {
+	timeOut := time.Date(
+		base.Year+mod.Year,
+		base.month+mod.month,
+		base.day+mod.day,
+		base.hour+mod.hour,
+		base.min+mod.min,
+		base.sec+mod.sec,
+		base.nsec,
+		base.loc)
+}
+
+func AddGigasecond(timeIn time.Time) time.Time {
+	giga := time.Date(31, 8, 6, 1, 46, 40, 0, time.UTC)
+
+	time := timeAdd(timeIn, giga)
+
+	return timeIn
+}
