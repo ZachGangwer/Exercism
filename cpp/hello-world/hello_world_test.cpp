6de3ded0b9399d754e84c971f4c22dcc358f0a24
Zach Gangwer <zach.gangwer@gmail.com>
2017-07-19 17:42:11 -0700

    CPP Submit - HelloWorld

--- cpp/hello-world/hello_world_test.cpp
+++ cpp/hello-world/hello_world_test.cpp
@@ -2,7 +2,6 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(test_hello)
-{
+BOOST_AUTO_TEST_CASE(test_hello) {
     BOOST_REQUIRE_EQUAL("Hello, World!", hello_world::hello());
 }
