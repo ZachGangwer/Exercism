7c51ecadfc17af79055081eda6ab0a62bf96e30b
Zach Gangwer <zach.gangwer@gmail.com>
2017-07-23 10:19:12 -0700

    PYTHON - RNA Transcription: DONE

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,16 @@
-def to_rna():
-    pass
+def to_rna(dna):
+    out = ""
+
+    for x in dna:
+        if x == 'G':
+            out += 'C'
+        elif x == 'C':
+            out += 'G'
+        elif x == 'T':
+            out += 'A'
+        elif x == 'A':
+            out += 'U'
+        else:
+            return ''
+
+    return out
