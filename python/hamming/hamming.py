92a42d875bbe8acc944466717c069ad5b7784ac8
Zach Gangwer <zach.gangwer@gmail.com>
2017-07-23 10:33:08 -0700

    PYTHON - Hamming: Submitted

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,14 @@
-def distance():
-    pass
+def distance(first, second):
+    count = 0;
+    i = 0;
+
+    if len(first) != len(second):
+        raise ValueError("Lengths differ: {} != {}", len(first), len(second));
+
+    while i < len(first):
+        if first[i] != second[i]:
+            count += 1;
+
+        i += 1;
+
+    return count;
