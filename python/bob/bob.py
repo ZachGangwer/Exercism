98d3de1f786cd0f478d881341152701beaa3d8a2
Zach Gangwer <zach.gangwer@gmail.com>
2017-07-25 20:55:38 -0700

    PYTHON - Bob: Submitted

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,11 @@
-def hey():
-    pass
+def hey(input):
+    input = input.strip()
+
+    if not input:
+        return "Fine. Be that way!"
+    elif input.isupper():
+        return "Whoa, chill out!"
+    elif input[len(input) - 1] == "?":
+        return "Sure."
+    else:
+        return "Whatever."
